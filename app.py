f858aeff2fc6a2e795f6d429125285c5ca6cf426
Ifeanyi Igweze <christopher.igweze@gmail.com>
2024-09-25 14:34:05 +0100

    set up the port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import os
 from flask import Flask, jsonify
 from question_loader import load_question_bank
 
@@ -10,5 +11,12 @@ def get_questions():
     # return jsonify(question_bank)  # Returns the entire question bank
     return jsonify(question_bank) # Returns the entire question bank
 
+# @app.route('/cognitive')
+# def get_cognitive_questions():
+#     question_bank = load_question_bank('questions.json')
+#     cognitive_questions = get_questions_by_category("Cognitive", question_bank)
+#     return jsonify(cognitive_questions)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the port from the environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
